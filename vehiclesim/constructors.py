2365639ad76b6e36db573e0a46cc81c088dde57a
antfc <cervantes_feng@outlook.com>
2020-01-16 14:23:50 +1000

    Colour coordinated GG Diagram

--- vehiclesim/constructors.py
+++ vehiclesim/constructors.py
@@ -1,5 +1,6 @@
 from vehiclesim import *
 
+
 def plotMassLapSim(mat_file, crv_name, window_w, window_h, g, m, P, p, A, Cd, mu, Cl):
     offset, crv_array, array_length, x_array, start_position, end_position, offset, crv = fetch_data(mat_file, crv_name)
     lat_velocity, peak_loc = lateral_velocity(g, m, P, p, A, Cd, mu, Cl, array_length, crv_array)
@@ -13,10 +14,7 @@ def plotMassLapSim(mat_file, crv_name, window_w, window_h, g, m, P, p, A, Cd, mu
 
     a_lat, a_long = lat_long(accel_a_lat, accel_a_long, brake_a_lat, brake_a_long, accel_velocity, velocity, crv)
 
-    graph_html, fig = plot_MassLap_all_html(window_w, window_h, x_array, crv, velocity, a_lat, a_long)
-
-    #pickle the 'image' to be called later to download
-    pickle.dump(fig, open("save.p", "wb"))
+    graph_html = plot_MassLap_all_html(window_w, window_h, x_array, crv, velocity, a_lat, a_long)
 
     return graph_html, fastest_lap, min_speed, max_speed
 
