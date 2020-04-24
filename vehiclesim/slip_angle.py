1fe81a0e2899fc0797615a93776b45cf4ed6ff37
Alex Lossberg <50721703+LDwhoRU@users.noreply.github.com>
2020-04-24 13:29:34 +1000

    Delete slip_angle.py

--- vehiclesim/slip_angle.py
+++
@@ -1,7 +0,0 @@
-from numpy import *
-
-def slip_angle(forward_velocity, lateral_velocity):
-	slip_angles = []
-	for i in range(forward_velocity):
-		slip_angles.append(arctan(lateral_velocity/forward_velocity)) * -1
-	return slip_angles
